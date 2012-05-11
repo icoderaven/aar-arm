0d2db0cbe49b859b737da0f6dce9f89ab77c7695
Kumar Shaurya Shankar <icoderaven@gmail.com>
2012-05-11 22:05:03 +0530

    Got the IK thing working, but there are reachability issues Also, perhaps the wrist length needs to be reduced to avoid self collision

--- scripts/src/try.py
+++ scripts/src/try.py
@@ -34,13 +34,31 @@ with env: # lock the environment since robot will be used
     traj = manipprob.MoveManipulator(goal=[0,0,0.2,0.5,0.6],execute=False, outputtrajobj=True)
     #Now try using inverse kinematics
     target=env.GetKinBody("cube")
-    destination_point=target.GetTransform()[0:3,3] #Extract only the translation vector from T
-    direction=array([0.0,0.0,1.0])  #Give it an approach direction, e.g. here Z axis
-    direction /= linalg.norm(direction) #Normalize the direction
+    destination_point=array([0.03,0.08,0.8])#
+    #destination_point = target.GetTransform()[0:3,3] #Extract only the translation vector from T
+#    direction=array([0.0,0.0,0.0])  #Give it an approach direction, e.g. here Z axis
+#    direction /= linalg.norm(direction) #Normalize the direction
     raveLogInfo("\nThe target Transform is "+repr(destination_point))
-    ikparam=IkParameterization(Ray(destination_point,direction), IkParameterization.Type.TranslationDirection5D)
+    
+    while True:
+        #direction = random.rand(3)-0.5
+        direction = destination_point-TEnd[0:3,3]
+        direction /= linalg.norm(direction)
+        #Need to assign a handle to ensure that the strip is visible
+        handle=env.drawlinestrip(array([destination_point,destination_point+0.1*direction]),10)
+        solutions = ikmodel.manip.FindIKSolutions(IkParameterization(Ray(destination_point,direction),IkParameterization.Type.TranslationDirection5D),IkFilterOptions.CheckEnvCollisions)
+        if solutions is not None and len(solutions) > 0: # if found, then break
+            break
+#    ikparam=IkParameterization(Ray(destination_point,direction), IkParameterization.Type.TranslationDirection5D)
+#    solution = ikmodel.manip.FindIKSolutions(ikparam,IkFilterOptions.CheckEnvCollisions)
+    raveLogInfo("\nThe solution for the ik Parameters is ")
+    if solutions is not None and len(solutions)>0:
+        print solutions
+        traj=manipprob.MoveManipulator(solutions[0],execute=False,outputtrajobj=True)
+    else:
+        RaveLogError("Couldn't find a solution!!!")
 raveLogInfo("And on...")
 robot.GetController().SetPath(traj)
 robot.WaitForController(0) # wait
 raveLogInfo("Ta!")
-time.sleep(3)
+time.sleep(20)
