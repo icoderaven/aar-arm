91697c1812db4f96aa209a55fbff25a62073726c
Kumar Shaurya Shankar <icoderaven@gmail.com>
2012-05-13 15:13:21 +0530

    Modified CAD model a. Introduced gap between wrist attachment and forearm b. Separated wrist attachment into wristX and wristZ to avoid precession of wrist attachment c. Modified gripper design to be hinged rather than sliding d. Tweaked a few parameters in er4u.xml e. Introduced cavities to accomodate for hinged gripper
    Movement of the arm now seems more smooth. Perhaps grasping target needs to be updated in er4u.xml

--- scripts/src/try.py
+++ scripts/src/try.py
@@ -8,7 +8,7 @@ env.Load('/home/kshaurya/aar-arm/robots/er4u.env.xml') # load a simple scene
 time.sleep(0.5) #wait for things to intialize
 robot = env.GetRobots()[0] # get the first robot
 
-RaveSetDebugLevel(DebugLevel.Debug) # set output level to debug
+RaveSetDebugLevel(DebugLevel.Verbose) # set output level to debug
 manipprob = interfaces.BaseManipulation(robot) # create the interface for basic manipulation programs
 manip = robot.GetActiveManipulator()
 
@@ -19,22 +19,21 @@ if not ikmodel.load():
 
 with env: # lock the environment since robot will be used
     raveLogInfo("Robot "+robot.GetName()+" has "+repr(robot.GetDOF())+" joints with values:\\n"+repr(robot.GetDOFValues()))
-    robot.SetDOFValues([0.1,-0.6,0.3,0.7,0.5],[0,1,2,3,4])
+    robot.SetDOFValues([0.5,-0.5,0.5,0.5,0.5,0.1],[0,1,2,3,4,5])
     env.UpdatePublishedBodies()
     time.sleep(1)
     T = robot.GetLinks()[1].GetTransform() # get the transform of link 1
     TEnd = manip.GetEndEffectorTransform()
     
-    robot.GetDOFValues
     raveLogInfo("The transformation of link 1 is:\n"+repr(T))
     raveLogInfo("\nThe transformation of end effector is:\n"+repr(TEnd))
     raveLogInfo("Moving on...")
     # call motion planner with goal joint angles
     raveLogInfo(str(len(robot.GetActiveManipulator().GetArmIndices())))
-    traj = manipprob.MoveManipulator(goal=[0,0,0.2,0.5,0.6],execute=False, outputtrajobj=True)
+    traj = manipprob.MoveManipulator(goal=[0,0,0.2,0.5,0.03],execute=False, outputtrajobj=True)
     #Now try using inverse kinematics
     target=env.GetKinBody("cube")
-    destination_point=array([0.03,0.08,0.8])#
+    destination_point=array([0.01,0.08,0.8])#
     #destination_point = target.GetTransform()[0:3,3] #Extract only the translation vector from T
 #    direction=array([0.0,0.0,0.0])  #Give it an approach direction, e.g. here Z axis
 #    direction /= linalg.norm(direction) #Normalize the direction
@@ -42,7 +41,7 @@ with env: # lock the environment since robot will be used
     
     while True:
         #direction = random.rand(3)-0.5
-        direction = destination_point-TEnd[0:3,3]
+        direction = [0,0,1]
         direction /= linalg.norm(direction)
         #Need to assign a handle to ensure that the strip is visible
         handle=env.drawlinestrip(array([destination_point,destination_point+0.1*direction]),10)
@@ -50,8 +49,8 @@ with env: # lock the environment since robot will be used
         if solutions is not None and len(solutions) > 0: # if found, then break
             break
 #    ikparam=IkParameterization(Ray(destination_point,direction), IkParameterization.Type.TranslationDirection5D)
-#    solution = ikmodel.manip.FindIKSolutions(ikparam,IkFilterOptions.CheckEnvCollisions)
     raveLogInfo("\nThe solution for the ik Parameters is ")
+#    solution = ikmodel.manip.FindIKSolutions(ikparam,IkFilterOptions.CheckEnvCollisions)
     if solutions is not None and len(solutions)>0:
         print solutions
         traj=manipprob.MoveManipulator(solutions[0],execute=False,outputtrajobj=True)
